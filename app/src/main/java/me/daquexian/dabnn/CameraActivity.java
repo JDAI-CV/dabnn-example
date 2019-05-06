25399afd083dc31a2d76169f08879abc8261b0e7
daquexian <daquexian566@gmail.com>
2019-05-06 13:09:45 +0800

    Remove Net to a separate module

--- app/src/main/java/me/daquexian/dabnn/CameraActivity.java
+++ app/src/main/java/me/daquexian/dabnn/CameraActivity.java
@@ -31,9 +31,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import me.daquexian.dabnn.Net;
 import pub.devrel.easypermissions.EasyPermissions;
 
 public class CameraActivity extends AppCompatActivity implements CvCameraViewListener2 {
